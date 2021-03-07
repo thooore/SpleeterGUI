4c756e401f1dd5de6519d92a4a1f662485e194c5
thooore <shitposterayy@gmail.com>
2021-03-07 01:03:26 +0100

    Added todo notes

--- SpleeterGui/Form1.cs
+++ SpleeterGui/Form1.cs
@@ -19,7 +19,7 @@ using System.Xml;
 
 /* TODO:
  * Make the panels a groupbox and disable all of them when running, so the user can't screw things up
- * 
+ * Refactor the Stem-building and ffmpeg stuff
  * 
  * 
  * */
